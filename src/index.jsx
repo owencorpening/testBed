76b77764ed23bd28ddc0799482261ac996307827
Owen Corpening <owen.corpening.contractor@saplinglearning.com>
2016-07-29 15:41:03 -0500

    missed two packages, didn't npm install --save

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,14 @@
 require("./../node_modules/bootstrap/dist/css/bootstrap.min.css")
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import ShowCommentSection from './core/screens/ShowCommentSection';
-import ShowTinyMCE from './core/screens/ShowtinyMCE';
+import ShowCommentSection from './core/screens/ShowCommentSection';
+// import ShowTinyMCE from './core/screens/ShowtinyMCE';
 
 export class App extends React.Component {
 	render()
 	{
 		return (
-			<ShowTinyMCE/>
+			<ShowCommentSection/>
 		);
 	}
 }
